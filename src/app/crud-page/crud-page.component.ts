f5eea82c178d5f39a4464075dedcd9b519dd3e6b
Luis Filipe Carrete <lfcsancehz@live.com>
2023-05-19 15:33:38 -0300

    crud completo

--- src/app/crud-page/crud-page.component.ts
+++ src/app/crud-page/crud-page.component.ts
@@ -23,15 +23,21 @@ export class CrudPageComponent {
   username: string = "";
   password: string = "";
 
+  editing: boolean = false;
+  editingId: number = 0;
+  editingname: string = ""
+  editingusername: string = "";
   ngOnInit() {
     var headers = { 'Authorization': `Bearer ${this.jwt["jwt"]}` }
 
     this.http.get<any>(environment.apiUrl + '/users', { headers }).subscribe(data => {
       this.dataSource = data;
+      this.dataSource = this.dataSource.sort((a: any, b: any) => a.id - b.id )
     },
       error => {
         alert("Trouble communicating with the server");
       })
+
   }
   deleteOnClick(id: number) {
     var headers = { 'Authorization': `Bearer ${this.jwt["jwt"]}` }
@@ -39,8 +45,32 @@ export class CrudPageComponent {
     this.http.delete<any>(environment.apiUrl + '/users/id/' + id, { headers }).subscribe(e => {})
     window.location.reload();
   }
+  editUser(id: number, name: string, username:string) {
+    this.editing = !this.editing;
+    this.editingId = id;
+    this.editingname = name;
+    this.editingusername = username;
+  }
+  editAddUser(id: number, name: string, username:string){
+    const body = {
+      id: id,
+      name: this.editingname,
+      username: this.editingusername,
+    }
+
+    var headers = { 'Authorization': `Bearer ${this.jwt["jwt"]}` }
+    this.http.patch<any>(environment.apiUrl+'/users/update', body, { headers: headers}).subscribe(
+      data => {window.location.reload();},
+      error => {alert("Something went wrong")});
+    
+    this.editing = false;
+  }
 
   submit(){
+    if(this.name.length < 1 || this.username.length < 1 || this.password.length < 1){
+      alert("All fields are required.");
+      return 
+    } 
     const body = {
       name: this.name,
       username: this.username,
