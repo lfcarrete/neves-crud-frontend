d6cc0306e2a64fa8dd215bef46a45f11177bfbc0
Luis Filipe Carrete <lfcsancehz@live.com>
2023-05-22 10:20:12 -0300

    redirect to crud if authenticated

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-login-page',
@@ -7,4 +10,15 @@ import { Component } from '@angular/core';
 })
 export class LoginPageComponent {
   title: string = 'Neves CRUD Basico';
+  constructor(
+    private http: HttpClient,
+    private router: Router){}
+
+  ngOnInit() {
+
+    this.http.get<any>(environment.apiUrl + '/users', { withCredentials: true }).subscribe(data => {
+      this.router.navigate(['crud']);
+    },)
+
+  }
 }
